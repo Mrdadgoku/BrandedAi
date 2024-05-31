8a54c1edcc35b8581a2be0311a23d781b00363bf
Mrdadgoku <dr084891@gmail.com>
2024-05-31 05:34:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 27353035))
-API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
-OWNER_ID = int(getenv("OWNER_ID", "6752444356"))
+API_ID = int(getenv("API_ID", 22008882))
+API_HASH = getenv("API_HASH", "cd2ee55895cb3a12830bf2998c413b28")
+OWNER_ID = int(getenv("OWNER_ID", "6462365139"))
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "BRANDED_WORLD")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "BRANDED_PAID_CC")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "BRANDEDKING82")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "BEST_FRIEND_FOREVER1230")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "Goku_Modz")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "GOKUxEDITION")
 
 # Random Start Images
 IMG = [
